a878c829e1ce7fbfb7cf7ad62372e81accbb5704
svensay <svensay@gmail.com>
2019-12-03 13:55:49 +0100

    Add Assets

---
+++ Assets/mouvementMoto.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class mouvementMoto : MonoBehaviour
+{	
+	public float moveSpeed = 10f;
+	public float turnSpeed = 50f;
+    // Start is called before the first frame update
+  
+    // Update is called once per frame
+    void Update()
+    {
+		if(Input.GetKey(KeyCode.D))
+			transform.Translate(10f,0,0);
+
+		if(Input.GetKey(KeyCode.Z))
+			transform.Translate(0,0,10f);
+
+		if(Input.GetKey(KeyCode.Q))
+			transform.Translate(-10f,0,0);
+
+		if(Input.GetKey(KeyCode.S))
+			transform.Translate(0,0,-10f);
+
+    }
+}
---
+++ Assets/validerBouton.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+	public class validerBouton : MonoBehaviour
+{
+	public GameObject canvas;
+	public static List<string> info;
+
+	 void Start()
+    {
+        //this.info.add("test");
+    }
+    // Start is called before the first frame update
+ // Détecter si un clic se produit
+    public void OnClick()
+    {
+            // Sortie pour consoler le nom du GameObject cliqué et le message suivant. Vous pouvez remplacer ceci par vos propres actions lorsque vous cliquez sur le GameObject .
+            Debug.Log ("objet de jeu cliqué avec le bouton droit de la souris!");
+			canvas.SetActive(false);
+    }
+}
