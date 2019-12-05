f5e8a6009dce44863c44c906f1c41e4e472a47cd
svensay <svensay@gmail.com>
2019-12-05 15:54:53 +0100

    Ajout du gps avec les routes et les objectifs + Récupération des infos dans la fiche dans le ganeObject Info

---
+++ Assets/probaAccident.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class probaAccident : MonoBehaviour
+{ 
+    public static List<string> info = new List<string>();
+    public static int nb_course;
+    public static int distance;
+    
+    public static Toggle currentToggle(ToggleGroup tg)
+    {
+        IEnumerator<Toggle> toggleEnum = tg.ActiveToggles().GetEnumerator();
+        toggleEnum.MoveNext();
+        return toggleEnum.Current;
+    }
+
+    public static void display_info()
+    {
+        foreach(string s in info)
+        {
+            Debug.Log(s);
+        }
+    }
+
+    public static void initInfo(ToggleGroup sexe, Dropdown age, Dropdown fp, Dropdown corp, ToggleGroup mc, Dropdown gravite_mc, ToggleGroup medoc, Dropdown gravite_medoc, Dropdown nb_course_init, Dropdown distance_init)
+    {
+        info.Add(currentToggle(sexe).name);
+        info.Add(age.options[age.value].text);
+        info.Add(fp.options[fp.value].text);
+        info.Add(corp.options[corp.value].text);
+        info.Add(currentToggle(mc).name);
+        info.Add(gravite_mc.options[gravite_mc.value].text);
+        info.Add(gravite_medoc.options[gravite_medoc.value].text);
+        info.Add(nb_course_init.options[nb_course_init.value].text);
+        info.Add(distance_init.options[distance_init.value].text);
+
+        display_info();
+    }
+
+
+
+}
--- Assets/validerBouton.cs
+++ Assets/validerBouton.cs
@@ -1,22 +1,33 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
-	public class validerBouton : MonoBehaviour
+public class validerBouton : MonoBehaviour
 {
-	public GameObject canvas;
-	public static List<string> info;
+    public ToggleGroup sexe;
+    public Dropdown age;
+    public Dropdown fp;
+    public Dropdown corp;
+    public ToggleGroup mc;
+    public Dropdown gravite_mc;
+    public ToggleGroup medoc;
+    public Dropdown gravite_medoc;
 
-	 void Start()
-    {
-        //this.info.add("test");
-    }
+    public Dropdown nb_course_init;
+    public Dropdown distance_init;
+
+    public GameObject fiche;
+    public GameObject player;
+
     // Start is called before the first frame update
- // Détecter si un clic se produit
+    // Détecter si un clic se produit
     public void OnClick()
     {
-            // Sortie pour consoler le nom du GameObject cliqué et le message suivant. Vous pouvez remplacer ceci par vos propres actions lorsque vous cliquez sur le GameObject .
-            Debug.Log ("objet de jeu cliqué avec le bouton droit de la souris!");
-			canvas.SetActive(false);
+        // Sortie pour consoler le nom du GameObject cliqué et le message suivant. Vous pouvez remplacer ceci par vos propres actions lorsque vous cliquez sur le GameObject .
+        Debug.Log("objet de jeu cliqué avec le bouton droit de la souris!");
+        probaAccident.initInfo(sexe, age, fp, corp, mc, gravite_mc, medoc, gravite_medoc, nb_course_init, distance_init);
+        fiche.SetActive(false);
+        player.AddComponent<mouvementMoto>();
     }
 }
